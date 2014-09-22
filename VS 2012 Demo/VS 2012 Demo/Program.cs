bdd589ad5ca951e50e55c8d691fa190561cad33d
Christopher Corliss <christopher.corliss@tetratech.com>
2014-09-22 17:32:19 -0400

    Initial VS 2012 commit

---
+++ VS 2012 Demo/VS 2012 Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS_2012_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
